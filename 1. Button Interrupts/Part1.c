35031c6a5d82cae6f0382b8690f96843c70cf6a3
Isaiah Roepke <roepke87@students.rowan.edu>
2023-02-18 08:59:21 -0500

    Parts 1 and 2
    Finished parts 1 and 2. Problem with 2 was that the interrupt was being configured incorrectly. Deleting 4 characters fixed the problem.

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -12,7 +12,7 @@
 
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
 
-void gpioInit()
+void gpioInit();
 
 
 int main(void)
@@ -35,10 +35,10 @@ int main(void)
 
     while(1)
     {
-        if (LEDColor)
+        if (LED_Color)
             P1OUT ^= BIT0;                  // P1.0 = toggle
         else
-            P6OUT &= ~BIT6;                 // Set P1.0 to 0
+            P6OUT ^= BIT6;                 // Set P1.0 to 0
         __delay_cycles(100000);
     }
 }
@@ -80,16 +80,21 @@ __interrupt void Port_2(void)
 {
     P2IFG &= ~BIT3;                         // Clear P1.3 IFG
 
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
+    if ((P2IES &= BIT3) == 0)       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
     {
         LED_Color = 0;
+        P1OUT &= ~BIT0;
         // @TODO Add code to change which edge the interrupt should be looking for next
+        P2IES |= BIT3;
+
     }
 
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
+    else if ((P2IES &= BIT3) == BIT3 ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
     {
         LED_Color = 1;
         // @TODO Add code to change which edge the interrupt should be looking for next
+        P6OUT &= ~BIT6;
+        P2IES &= ~BIT3;
     }
 }
 
--- 2. Timers/Part2.c
+++ 2. Timers/Part2.c
@@ -12,32 +12,60 @@
 void gpioInit();
 void timerInit();
 
-void main(){
+char LED_Speed = 0x00;
 
-    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
 
-    gpioInit();
-    timerInit();
+gpioInit();
 
-    // Disable the GPIO power-on default high-impedance mode
-    // to activate previously configured port settings
+timerInit();
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
     PM5CTL0 &= ~LOCKLPM5;
 
-    __bis_SR_register(LPM3_bits | GIE);
+
+    __bis_SR_register(GIE);         // Enter LPM3 w/ interrupt
+
+    while (1){
+
+        if (LED_Speed == 0x00){
+             TB1CTL = TBSSEL_1 | MC_2  | ID_2;
+        }
+        else if (LED_Speed == 0x01){
+
+            TB1CTL = TBSSEL_1 | MC_2  | ID_1;
+        }
+        else if (LED_Speed == 0x02){
+             TB1CTL = TBSSEL_1 | MC_2  | ID_0;
+        }
+
+
+
+    }
 
 }
 
-
 void gpioInit(){
-    // @TODO Initialize the Red or Green LED
-
+    // @TODO Initialize the Red OR Green LED
+    P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
+    P1DIR |= BIT0;                          // Set P1.0 to output direction
     // @TODO Initialize Button 2.3
+    P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
+    P2REN |= BIT3;                          // P2.3 pull-up register enable
+    P2IES |= BIT3;                         // P2.3 High to low edge/active low
+    P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 
 }
 
 void timerInit(){
     // @TODO Initialize Timer B1 in Continuous Mode using ACLK as the source CLK with Interrupts turned on
+    // Timer1_B3 setup
+    TB1CCTL0 = CCIE;                          // TBCCR0 interrupt enabled
+    TB1CCR0 = 32768;
+    TB1CTL = TBSSEL_1 | MC_2 | TBCLR | ID_2 | TBIE ;                 // ACLK, continuous mode
 
 }
 
@@ -51,17 +79,51 @@ void timerInit(){
 __interrupt void Port_2(void)
 {
     // @TODO Remember that when you service the GPIO Interrupt, you need to set the interrupt flag to 0.
-
+    P2IFG &= ~BIT3;
     // @TODO When the button is pressed, you can change what the CCR0 Register is for the Timer. You will need to track what speed you should be flashing at.
+    if ((P2IES &= BIT3) == BIT3){
+        switch (LED_Speed){
+            case 0x00:
+                LED_Speed = 0x01;
+
+                break;
+            case 0x01:
+                LED_Speed = 0x02;
+
+                break;
+            case 0x02:
+                LED_Speed = 0x00;
+
+                break;
+        }
+    }
 
 }
 
+/*
+//TIMER0_B1_VECTOR
 
+#pragma vector=TIMER0_B1_VECTOR
+__interrupt void TB1_ISR(void)
+{
+    TB1CTL &= TBIFG ;
+    P1OUT ^= BIT0;
+    TB1CCR0 += 32768;                         // Add Offset to TB1CCR0
+
+
+}
+*/
 // Timer B1 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector = TIMER1_B0_VECTOR
 __interrupt void Timer1_B0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(TIMER1_B0_VECTOR))) Timer1_B0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
 {
-    // @TODO You can toggle the LED Pin in this routine and if adjust your count in CCR0.
+    P1OUT ^= BIT0;
+    TB1CCR0 += 32768;                         // Add Offset to TB1CCR0
 }
-
 
